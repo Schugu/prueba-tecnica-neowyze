6e3f5665623f461d6a64d4066890e73f3fdedfb4
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2024-05-13 16:01:14 -0300

    Refactor: Cambio de img

--- app/page.jsx
+++ app/page.jsx
@@ -5,10 +5,11 @@ export default function Home() {
   return (
     <main className="flex flex-col items-center gap-4 p-4 min-h-screen">
       <Image
-      width={700}
-      height={300}
-      src='/BabyYoda.jpg'
-      alt="Portada"
+        width={700}
+        height={300}
+        src='/star-wars.jpg'
+        alt="Portada"
+        className="w-2/5"
       ></Image>
       <p className="indent-16 inline-block">
         En una galaxia lejana, en un tiempo de conflictos interminables y héroes
@@ -28,12 +29,12 @@ export default function Home() {
       </p>
 
       <p className="indent-16 inline-block">
-        En este universo de aventuras sin fin, donde los saberes antiguos se entrelazan 
-        con la tecnología más avanzada, donde la amistad y el amor desafían al odio y 
-        la desesperación, cada paso es una elección que puede cambiar el curso de la 
-        historia. Y así, la saga de Star Wars continúa, una epopeya intergaláctica que 
-        cautiva la imaginación de generaciones, recordándonos que incluso en los 
-        momentos más oscuros, la luz siempre encuentra un camino para brillar. 
+        En este universo de aventuras sin fin, donde los saberes antiguos se entrelazan
+        con la tecnología más avanzada, donde la amistad y el amor desafían al odio y
+        la desesperación, cada paso es una elección que puede cambiar el curso de la
+        historia. Y así, la saga de Star Wars continúa, una epopeya intergaláctica que
+        cautiva la imaginación de generaciones, recordándonos que incluso en los
+        momentos más oscuros, la luz siempre encuentra un camino para brillar.
         Que la Fuerza esté contigo, siempre.
       </p>
     </main>
