7a945eaf2594627d8a1d6a6cedb364ddd88c8a6a
Leandro Daniel Schugurensky <47997052+Schugu@users.noreply.github.com>
2024-05-13 18:39:28 -0300

    Add: Centré texto del footer

--- app/components/footer/Footer.jsx
+++ app/components/footer/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <section className="w-full p-4 border-t-2 border-yellow-500 flex
     items-center justify-center">
-      <p tabIndex={1000} className="inline-block">Página creada por: <Link href='https://github.com/Schugu' className="text-yellow-500 
+      <p tabIndex={1000} className="inline-block text-center">Página creada por: <Link href='https://github.com/Schugu' className="text-yellow-500 
         hover:border-b hover:border-yellow-600"
         >Leandro Daniel Schugurensky</Link></p>
     </section>
